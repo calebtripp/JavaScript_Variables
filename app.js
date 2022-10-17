d8d86f37110215bfad8526994e7b3508775beae3
calebtripp <calebtripp@outlook.com>
2022-10-17 11:18:18 -0400

    All Exercises Complete

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-console.log("Hello World!\n==========\n");
-console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-);
-
-// Exercise 1
+// Exercises 1 & 2
 const firstName = "Caleb";
 let lastName = "Tripp";
 var age = 37;
 console.log(firstName, lastName, age)
 
-// Exercise 2
-//done within exercise 1
-
 // Exercise 3
 let language = "JavaScript";
 let createdYear = 1995;
 let isCaseSensitive = true;
+console.log(language, createdYear, isCaseSensitive)
 
 //Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+console.log(price, isOnSale, salePercentage, stock, inStock, selectedSize)
 
+//Exercise 5
+let title = "Name Of The Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+console.log(title, author, pageCount, bookmark, hasRead)

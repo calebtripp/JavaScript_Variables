920be2c947ffd55b3e6023c4d1f9e1a586652f37
calebtripp <calebtripp@outlook.com>
2022-10-17 11:03:29 -0400

    Exercise 3 Complete

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ console.log("Hello World!\n==========\n");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
-console.log()
+
 // Exercise 1
 const firstName = "Caleb";
 let lastName = "Tripp";
 var age = 37;
-
+console.log(firstName, lastName, age)
 
 // Exercise 2
+//done within exercise 1
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+//Exercise 4
+
